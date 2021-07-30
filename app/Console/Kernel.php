cb4a2a5d9ca51726b51e7ecee3ad93a717913b0c
John Azar (PC) <>
2021-07-30 23:37:00 +0300

    command to cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        // add your command to cron job
+        // $schedule->command('mycommand')->dailyAt('8:00');
     }
 
     /**
