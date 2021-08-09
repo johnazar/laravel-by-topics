896f81f35f5b7ce2167f6d51a06c51fffe49c540
John Azar (PC) <>
2021-08-09 16:18:48 +0300

    ViewTestServiceProvider

--- app/Providers/ViewTestServiceProvider.php
+++ app/Providers/ViewTestServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class TestServiceProvider extends ServiceProvider
+class ViewTestServiceProvider extends ServiceProvider
 {
     /**
      * Register services.
