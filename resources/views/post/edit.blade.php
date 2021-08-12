722a0d49a6801af74f99d49d3503aeceaace0e58
John Azar (PC) <>
2021-08-12 16:42:20 +0300

    send put request

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -8,8 +8,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form action="{{route('posts.store')}}" method="post" class="space-y-6">
+                    <form action="{{route('posts.update',$post->id)}}" method="post" class="space-y-6">
                         @csrf
+                        @method('PUT')
                         <label for="title">{{__('Post title')}}</label>
                         <input type="text" name="title" id="post_title" value="{{$post->title}}" class="w-full">
                         <x-button>{{__('Create')}}</x-button>
