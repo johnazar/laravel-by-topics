a0d12edc419a37047661a3b2855053b330944f4e
John Azar (PC) <johnazar@users.noreply.github.com>
2021-08-14 23:17:22 +0300

    post softdelete

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,11 +7,12 @@ use App\Models\Comment;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pipeline\Pipeline;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
     protected $guarded =[];
 
     /**
--- database/migrations/2021_08_04_185153_create_posts_table.php
+++ database/migrations/2021_08_04_185153_create_posts_table.php
@@ -18,6 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->datetime('published_at')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
